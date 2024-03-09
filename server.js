b8b89ec9438aba864f072af386e801e739127e86
Ágatha <agathamagnusg@hotmail.com>
2024-03-09 10:33:45 -0300

    ✨ Implement cache for product listing with Redis
    Adicionada lógica de cache no endpoint de listagem de produtos para reduzir tempos de carregamento e diminuir a dependência de fontes de dados externas ou bancos de dados. A implementação tenta buscar os dados dos produtos no cache do Redis usando a chave "getAllProducts". Caso os dados não estejam disponíveis no cache, busca dados frescos a partir de uma operação simulada, armazena esses dados no Redis para requisições futuras e, então, serve os dados ao usuário. Esta abordagem melhora o desempenho ao servir dados em cache quando disponíveis, recorrendo à recuperação de dados frescos somente quando necessário.

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 
 const { createClient } = require('redis');
-const client = createClient();
+const client = createClient(
+    {
+        host: 'redis-server',
+        port: 6379,
+    }
+);
 
 const getAllProducts = async () => {
     const time = Math.random() * 5000;
@@ -14,15 +19,24 @@ const getAllProducts = async () => {
 };
 
 app.get('/', async (req, res) => {
+    const productsFromCache = await client.get("getAllProducts");
+    if (productsFromCache) {
+        return res.send(JSON.parse(productsFromCache));
+    }
     const products = await getAllProducts();
+    await client.set("getAllProducts", JSON.stringify(products));
     res.send(products);
 })
 
-const startup = async () => {  
-    await client.connect(); 
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
-}
+const startup = async () => {
+    try {
+        await client.connect();
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    } catch (error) {
+        console.error('Falha ao conectar no Redis:', error);
+    }
+};
 
 startup();
